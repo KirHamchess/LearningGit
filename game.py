ea2cfbe7de4cb8501698583f6e60591f73143b96
KirHamchess <vrpatutin@gmail.com>
2023-02-09 17:52:24 +0400

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random
-secret = random.randint(1, 500)
+secret = random.randint(1, 100)
 guess = 0
 tries = 0
 print ("Компьютер: Привет! давно не виделись. Как же я по тебе скучал! Давай поиграем!")
 print ("Вы: А во что?")
-print ("Компьютер: В числа! числа от 1 до 500!")
+print ("Компьютер: В числа! числа от 1 до 100!")
 print("Вы: Ну, давай попробуем.")
-while guess != secret and tries < 6:
-    guess = int(input("скажи число:"))
+while guess != secret and tries < 10:
+    guess = int(input("число:"))
     if guess < secret:
         print ("слишком мало!")
     elif guess > secret:
